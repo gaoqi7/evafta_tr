9962866b3b7661410311c6aa560d713f7c3359b8
Rick Gao <gaoqi7@gmail.com>
2024-04-17 09:30:09 -0700

    add index.js file and test the submit function

---
+++ index.js
@@ -0,0 +1,12 @@
+const gsID = "1fQMVt7QOhwGNvLtzAlEc-HyiQU7xAY0YoKvGj2gU_Qo"
+const gsTabRecord = "tr"
+
+let flightRecordForm = document.getElementById("flightRecordForm")
+
+flightRecordForm.addEventListener("submit",(e)=>{
+
+    e.preventDefault();
+    let cadetID = document.getElementById("cadetID").value
+    let record = document.getElementById("record").value
+    console.log(`this student ${cadetID} 's record is ${record}`)
+})
