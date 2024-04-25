4d01ab3adeea8efe7d7555ddbdb8bb7be8654cc4
gaoqi7@gmail.com <gaoqi7@gmail.com>
2024-04-25 16:09:18 -0700

    add pocketbase

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const gsID = "1fQMVt7QOhwGNvLtzAlEc-HyiQU7xAY0YoKvGj2gU_Qo"
-const gsTabRecord = "tr"
-
 let flightRecordForm = document.getElementById("flightRecordForm")
 
 flightRecordForm.addEventListener("submit",(e)=>{
