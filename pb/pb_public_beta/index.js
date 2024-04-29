4945e05917608b97ee7ea38d28eec01aca3f72b6
Rick Gao <gaoqi7@gmail.com>
2024-04-29 10:43:34 -0700

    add query by ID function

--- pb/pb_public_beta/index.js
+++ pb/pb_public_beta/index.js
@@ -1,7 +1,5 @@
 const pb = new PocketBase("http://127.0.0.1:8090")
     pb.admins.authWithPassword('gaoqi7@gmail.com','2020Jul03-pb')
-console.log(pb.authStore.isValid)
-console.log(pb.authStore.token)
 let flightRecordForm = document.getElementById("flightRecordForm")
 
 flightRecordForm.addEventListener("submit",(e)=>{
@@ -14,4 +12,18 @@ flightRecordForm.addEventListener("submit",(e)=>{
         "record": record
     }
     pb.collection('evafta_tr').create(data)
+
+
+})
+let trSearchByID = document.getElementById("trSearchByID")
+
+trSearchByID.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let quaryID = document.getElementById("quaryID").value
+    console.log(quaryID)
+    quaryResult = pb.collection('evafta_tr').getList(1,10,{
+        filter: `cadetID = '${quaryID}'`
+    }).then(data=>{
+        console.log(data.items[0].record)
+    })
 })
