5c86dffadf63c7f5e493ddba52922186ed44d105
rick <gaoqi7@gmail.com>
2024-05-01 23:18:21 -0700

    add several css framework, I like pico css

--- webpage/index.js
+++ webpage/index.js
@@ -1,34 +1,30 @@
-const pb = new PocketBase("http://127.0.0.1:8090")
-    pb.admins.authWithPassword('gaoqi7@gmail.com','2020Jul03-pb')
-let flightRecordForm = document.getElementById("flightRecordForm")
+const pb = new PocketBase("http://127.0.0.1:8090");
+pb.admins.authWithPassword("gaoqi7@gmail.com", "2020Jul03-pb");
+let flightRecordForm = document.getElementById("flightRecordForm");
 
-flightRecordForm.addEventListener("submit",(e)=>{
+flightRecordForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let cadetID = document.getElementById("cadetID").value;
+  let record = document.getElementById("record").value;
+  const data = {
+    cadetID: cadetID,
+    record: record,
+  };
+  pb.collection("evafta_tr").create(data);
+});
+let trSearchByID = document.getElementById("trSearchByID");
 
-    e.preventDefault();
-    let cadetID = document.getElementById("cadetID").value
-    let record = document.getElementById("record").value
-    const data={
-        "cadetID": cadetID,
-        "record": record
-    }
-    pb.collection('evafta_tr').create(data)
-
-})
-let trSearchByID = document.getElementById("trSearchByID")
-
-trSearchByID.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    let queryID = document.getElementById("queryID").value
-    console.log(queryID)
-    quaryResult = pb.collection('evafta_tr').getList(1,10,{
-        filter: `cadetID = '${queryID}'`
-    }).then(data=>{
-    document.getElementById("queryResultID").innerHTML = `${queryID}`
-    document.getElementById("queryResultRecord").innerHTML = `${data.items[0].record}`
-})
+trSearchByID.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let queryID = document.getElementById("queryID").value;
+  console.log(queryID);
+  quaryResult = pb
+    .collection("evafta_tr")
+    .getList(1, 10, {
+      filter: `cadetID = '${queryID}'`,
     })
-    
-        
-
-
-
+    .then((data) => {
+      document.getElementById("queryResultID").innerHTML = `${queryID}`;
+      document.getElementById("queryResultRecord").innerHTML = `${data.items[0].record}`;
+    });
+});
